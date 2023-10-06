9aeae74cf75165c2ccc0054c5fa7cfbdae6cc0d5
elisabethviolinaa <elisabethviolina@gmail.com>
2023-10-07 02:52:58 +0700

    styling user's page

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,14 +1,21 @@
-$(".sidebar ul li").on('click', function () {
-    $(".sidebar ul li.active").removeClass('active');
-    $(this).addClass('active');
-});
+// add hovered class to selected list item
+let list = document.querySelectorAll(".sidenav li");
 
-$('.open-btn').on('click', function () {
-    $('.sidebar').addClass('active');
+function activeLink() {
+  list.forEach((item) => {
+    item.classList.remove("hovered");
+  });
+  this.classList.add("hovered");
+}
 
-});
+list.forEach((item) => item.addEventListener("mouseover", activeLink));
 
-$('.close-btn').on('click', function () {
-    $('.sidebar').removeClass('active');
+// Menu Toggle
+let toggle = document.querySelector(".toggle");
+let sidenav = document.querySelector(".sidenav");
+let outer = document.querySelector(".outer");
 
-})
+toggle.onclick = function () {
+  sidenav.classList.toggle("active");
+  outer.classList.toggle("active");
+};
